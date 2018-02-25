24a1ada2ba9a008d107600e8c3f9e537394efccb
Ryan Mills <ryandarylmills@gmail.com>
2018-02-25 14:32:29 +1100

    Class.

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
+class HelloWorld(object):
+    def get(self):
+        return {'hello': 'world'}
+
 @app.route("/")
 def hello():
     return "Hello from Python!"
